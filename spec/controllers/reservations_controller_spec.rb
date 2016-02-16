e2ae503f02041f87de369a3d22dbad8c593124c9
David Faulkenberry <david.faulkenberry@gmail.com>
2016-02-16 15:27:29 -0500

    Finishing create specs

--- spec/controllers/reservations_controller_spec.rb
+++ spec/controllers/reservations_controller_spec.rb
@@ -19,16 +19,39 @@ describe ReservationsController do
           .to receive(:find_available)
           .with(start_datetime, end_datetime)
           .and_return item
-      end
-      it 'calls #reserve! on the item' do
         expect_any_instance_of(Item)
           .to receive(:reserve!)
           .with(start_datetime, end_datetime)
           .and_return reservation
+      end
+      it 'calls #reserve! on the item' do
         submit
       end
+      it 'has an OK status' do
+        submit
+        expect(response).to have_http_status :ok
+      end
+      it 'responds with an object containing the reservation ID' do
+        submit
+        json = JSON.parse response.body
+        expect(json).to eql 'id' => reservation.id
+      end
     end
-    context 'with unavailable item' do
+    context 'with no available item' do
+      before :each do
+        expect_any_instance_of(ItemType)
+          .to receive(:find_available)
+          .with(start_datetime, end_datetime)
+          .and_return nil
+      end
+      it 'has an unprocessable entity status' do
+        submit
+        expect(response).to have_http_status :unprocessable_entity
+      end
+      it 'has an empty response body' do
+        submit
+        expect(response.body).to be_empty
+      end
     end
   end
 end
