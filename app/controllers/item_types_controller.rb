4a47869c464d7cf4936ece22f4b633803f3acdab
David Faulkenberry <david.faulkenberry@gmail.com>
2016-02-16 21:46:57 -0500

    Remove unneeded return

--- app/controllers/item_types_controller.rb
+++ app/controllers/item_types_controller.rb
@@ -1,6 +1,6 @@
 class ItemTypesController < ApplicationController
   def index
     render json: ItemType.order(:name), only: [:name, :allowed_keys],
-           include: { items: { only: :name } } and return
+           include: { items: { only: :name } }
   end
 end
