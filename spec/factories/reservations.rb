244f75e4fd9fe33a83435585d26df22513b712e2
David Faulkenberry <david.faulkenberry@gmail.com>
2016-02-17 16:14:59 -0500

    Fix Travis with parentheses

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -6,22 +6,22 @@ FactoryGirl.define do
   end
 
   trait :starting_in_default_range do
-    start_datetime default_start_time + 1.day
-    end_datetime default_end_time + 1.day
+    start_datetime(default_start_time + 1.day)
+    end_datetime(default_end_time + 1.day)
   end
 
   trait :ending_in_default_range do
-    start_datetime default_start_time - 1.day
-    end_datetime default_end_time - 1.day
+    start_datetime(default_start_time - 1.day)
+    end_datetime(default_end_time - 1.day)
   end
 
   trait :spanning_default_range do
-    start_datetime default_start_time - 1.day
-    end_datetime default_end_time + 1.day
+    start_datetime(default_start_time - 1.day)
+    end_datetime(default_end_time + 1.day)
   end
 
   trait :not_during_default_range do
-    start_datetime default_start_time - 1.week
-    end_datetime default_start_time - 6.days
+    start_datetime(default_start_time - 1.week)
+    end_datetime(default_start_time - 6.days)
   end
 end
