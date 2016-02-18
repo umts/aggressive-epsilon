df2eb0099808c5a8aeff691eb45cc717ce94a12a
David Faulkenberry <david.faulkenberry@gmail.com>
2016-02-18 16:09:32 -0500

    Item routing and controller

---
+++ app/controllers/v1/items_controller.rb
@@ -0,0 +1,43 @@
+module V1
+  class ItemsController < ApplicationController
+    before_action :find_item, only: %i(destroy show update)
+
+    def create
+      item = Item.new params.permit(:name, :item_type_id, data: {})
+      if item.save
+        render json: item, except: [:created_at, :updated_at]
+      else render json: { errors: item.errors.full_messages },
+                  status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @item.destroy
+      render nothing: true
+    end
+
+    def index
+      item_type = ItemType.find_by id: params.require(:item_type_id)
+      render json: item_type.items.order(:name), except: [:created_at, :updated_at]
+    end
+
+    def show
+      render json: @item, except: [:created_at, :updated_at]
+    end
+
+    def update
+      changes = params.require(:item).permit :name, :item_type_id, data: {}
+      if @item.update changes then render nothing: true
+      else render json: { errors: @item.errors.full_messages },
+                  status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def find_item
+      @item = Item.find_by id: params.require(:id)
+      render nothing: true, status: :not_found unless @item.present?
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   namespace :v1 do
-    resources :item_types, except: [:new, :edit]
-    resources :reservations, except: [:new, :edit] do
+    resources :item_types, except: %i(new edit)
+    resources :items, except: %i(new edit)
+    resources :reservations, except: %i(new edit) do
       member { post :update_item }
     end
   end
