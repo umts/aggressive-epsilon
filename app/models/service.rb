cda7c968cac6d67d679a1e8fb490b8d0c52fa18a
David Faulkenberry <david.faulkenberry@gmail.com>
2016-02-22 09:18:28 -0500

    Inline aliases

--- app/models/service.rb
+++ app/models/service.rb
@@ -8,20 +8,17 @@ class Service < ActiveRecord::Base
 
   def can_edit?(reservation)
     created?(reservation) || can_write_to?(reservation.item_type)
-  end
-  alias able_to_edit? can_edit?
+  end; alias able_to_edit? can_edit?
 
   def can_read?(item_type)
     permissions.find_by(item_type: item_type).present?
-  end
-  alias able_to_read? can_read?
+  end; alias able_to_read? can_read?
 
   # When called generically (service.can_write?), this checks whether the
   # service has write permissions to ANY item type (**not** to all item types).
   def can_write_to?(item_type = ItemType.all)
     permissions.find_by(item_type: item_type, write: true).present?
-  end
-  alias able_to_write_to? can_write_to?
+  end; alias able_to_write_to? can_write_to?
 
   def created?(reservation)
     reservation.creator == self
