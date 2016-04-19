a3cdcec0a4acf08a8bd5cc1312b959f24a2111ae
Tom Peck <tapeck@umass.edu>
2016-04-19 12:17:06 -0400

    seeds file update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,13 @@ require 'rspec'
 require 'rubygems'
 include ReservationHelper
 
-unless Service.find_by url: 'localhost'
-  service = FactoryGirl.create :service, name: 'dev stuff', url: 'localhost'
-  item_type = FactoryGirl.create :item_type, name: 'programmer', creator: service
+@service = Service.find_by url: 'localhost'
+unless @service
+  @service = FactoryGirl.create :service, name: 'dev stuff', url: 'localhost'
+  item_type = FactoryGirl.create :item_type, name: 'programmer', creator: @service
   item = FactoryGirl.create :item, name: 'Tom', reservable: true, item_type: item_type
   FactoryGirl.create :reservation, item: item, start_datetime: Time.parse('15/1/11').iso8601, 
-                                   end_datetime: Time.parse('18/1/5').iso8601, creator: service
+                                   end_datetime: Time.parse('18/1/5').iso8601, creator: @service
 end
+
+puts "Your api_key is: " + @service.api_key
