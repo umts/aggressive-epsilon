a8e0643a6bd4eb23ebcf6cf91cdc0b10ed911e30
Tom Peck <tapeck@umass.edu>
2016-05-03 15:47:18 -0400

    updated the names of the seeds to be a bit better

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@ require 'rails_helper'
 service = Service.find_by url: 'localhost'
 unless service
   service = FactoryGirl.create :service, name: 'dev stuff', url: 'localhost'
-  item_type = FactoryGirl.create :item_type, name: 'programmer', creator: service
-  item = FactoryGirl.create :item, name: 'Tom', reservable: true, item_type: item_type
+  item_type = FactoryGirl.create :item_type, name: 'TEST_CREATE_RENTAL_TYPE', creator: service
+  item = FactoryGirl.create :item, name: 'TEST_CREATE_RENTAL_ITEM', reservable: true, item_type: item_type
   FactoryGirl.create :reservation, item: item, start_datetime: Time.parse('15-1-11').iso8601, 
                                    end_datetime: Time.parse('18-1-5').iso8601, creator: service
 end
