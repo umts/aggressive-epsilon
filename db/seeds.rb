af727080bb3169bfe9aa001dbc860008feca6368
Tom Peck <tapeck@umass.edu>
2016-05-10 15:52:11 -0400

    a little edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,8 @@ service = Service.find_by url: 'localhost'
 unless service
   service = FactoryGirl.create :service, name: 'dev stuff', url: 'localhost'
   item_type = FactoryGirl.create :item_type, name: 'TEST_ITEM_TYPE', creator: service
-  another_item_type = FactoryGirl.create :item_type, name: 'TEST_ITEM_TYPE_2', creator: service
   # now we're going to need a lot of items to test with
-  FactoryGirl.create :item, name: 'TEST_CREATE_RENTAL_ITEM', reservable: true, item_type: item_type
-  rn = lambda {
-    name = ''
-    name += (rand(26)+65).chr until name.length > 120
-    name
-  }
-  FactoryGirl.create :item, name: rn.call, reservable: true, item_type: item_type until Item.count > 40
+  FactoryGirl.create :item, reservable: true, item_type: item_type until Item.count > 40
 end
 
 puts "Your api_key is: " + service.api_key
