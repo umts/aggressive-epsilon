59a01b48f6ac919dbd88f2624c23d57fe5e4718c
G. Logan Slinski <phantom.patrol@gmail.com>
2016-06-16 14:17:04 -0400

    Adding just a bit more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,15 @@ exit if Rails.env.test?
 service = Service.find_by url: 'localhost'
 unless service
   service = FactoryGirl.create :service, name: 'dev stuff', url: 'localhost'
-  item_type = FactoryGirl.create :item_type, name: 'TEST_ITEM_TYPE', creator: service
+
+  test_type = FactoryGirl.create :item_type, name: 'TEST_ITEM_TYPE', creator: service
+  10.times { FactoryGirl.create :item, reservable: true, item_type: test_type }
+  four_seater = FactoryGirl.create :item_type, name: '4 Seat', creator: service
+  10.times { FactoryGirl.create :item, reservable: true, item_type: four_seater }
+  six_seater = FactoryGirl.create :item_type, name: '6 Seat', creator: service
+  10.times { FactoryGirl.create :item, reservable: true, item_type: six_seater }
+
   # now we're going to need a lot of items to test with
-  40.times { FactoryGirl.create :item, reservable: true, item_type: item_type }
 end
 
 puts "Your api_key is: " + service.api_key
